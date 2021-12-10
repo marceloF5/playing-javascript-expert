9ac531033be363ed23d054dd9eff08440f5d1ce1
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-12-10 20:07:46 +0000

    chore: Remove .zip

--- aula01-mocks/index.test.js
+++ aula01-mocks/index.test.js
@@ -1,8 +1,8 @@
 const { error } = require('./src/constants')
 const File = require('./src/file')
 const { rejects, deepStrictEqual } = require('assert')
-    ;
-(async () => {
+    
+;(async () => {
     {
         const filePath = './mocks/emptyFile-invalid.csv'
         const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
